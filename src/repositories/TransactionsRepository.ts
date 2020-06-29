f60968926645e786d0e5476abbe09280f16fcbf6
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2020-06-29 16:35:39 -0300

    Adicionando outra maneira de fazer o getBalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,6 +24,27 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
+    // const { income, outcome } = this.transactions.reduce(
+    //   (accumulator: Balance, transaction: Transaction) => {
+    //     switch (transaction.type) {
+    //       case 'income':
+    //         accumulator.income += transaction.value;
+    //         break;
+    //       case 'outcome':
+    //         accumulator.outcome += transaction.value;
+    //         break;
+    //       default:
+    //         break;
+    //     }
+    //     return accumulator;
+    //   },
+    //   {
+    //     income: 0,
+    //     outcome: 0,
+    //     total: 0,
+    //   },
+    // );
+
     const income = this.transactions
       .filter(transaction => transaction.type === 'income')
       .reduce(
